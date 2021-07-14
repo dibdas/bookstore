d15d1e408b8d6e5db888201a1615470423e4910a
dibdas <dibyandu08das@gmail.com>
2021-07-14 18:24:51 +0530

    added index.js
    added cretebook , removebook

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+const CREATEBOOK = 'CREATE_BOOK';
+const REMOVEBOOK = 'REMOVE_BOOK';
+
+const createbook = (book) => {
+  CREATEBOOK,
+  book;
+};
+
+const removebook =(book)=>{
+    REMOVEBOOK,
+    book;
+}
+
+export const {CREATEBOOK,REMOVEBOOK,createbook,removebook}
