8a3ade4972de0eb6f56396c2e79be8168f12833e
dibdas <dibyandu08das@gmail.com>
2021-07-15 18:44:28 +0530

    modified
    added options

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -4,6 +4,30 @@ class BooksList extends React.Component {
   constructor(props) {
     super(props);
   }
-}
+  render {
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h3>Book Id</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h3>Title</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h3>Category</h3>
+          </div>
+        </div>
+      </div>
+
+
+    )
+  }
+
+};
 
 export default BooksList;
