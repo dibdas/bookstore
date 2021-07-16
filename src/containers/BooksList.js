e03f6f3e28bc7c6afaf159e09568c5712773fee6
dibdas <dibyandu08das@gmail.com>
2021-07-16 19:13:10 +0530

    modified
    added mapStateToprops

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,13 +1,18 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import propTypes from 'prop-types';
+import { removebook } from '../actions/index';
+import Book from '../components/Book';
 
-class BooksList extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props);
+const BooksList = (props)=> {
+  const {removebook, books} =props
+
+  handleRemoveBook = (book) => {
+    removebook(book.Id);
   }
 
   render() {
+   
     return (
       <div className="container">
         <div className="row">
@@ -25,10 +30,34 @@ class BooksList extends React.Component {
             <h3>Category</h3>
           </div>
         </div>
+        {books.map((book) => (
+          <Book
+            book={book}
+            key={book.Id}
+            handleRemoveBook={handleRemoveBook}
+          />
+        )) }
       </div>
 
     );
   }
 }
+BooksList.propTypes = {
+  books: propTypes.arrayOf(propTypes.object).isRequired,
+  removebook: propTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    books: state.books
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: () => {
+      dispatch(addItem())
+    }
+  };
+};
 
 export default BooksList;
