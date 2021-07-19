756f015f2207a83a25b54a7a0d79a277eb7bea9b
dibdas <dibyandu08das@gmail.com>
2021-07-20 00:46:14 +0530

    modified
    fixed linters

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,16 +2,16 @@ const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
 const createbook = (title,
-  category) => {
-  CREATE_BOOK;
+  category) => ({
+  type: CREATE_BOOK,
   title,
-  category;
-};
+  category,
+});
 
-const removebook = (id) => {
-  REMOVE_BOOK;
-  id;
-};
+const removebook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
 
 export {
   CREATE_BOOK, REMOVE_BOOK, createbook, removebook,
--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/no-unused-state */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -27,10 +26,10 @@ class BooksForm extends React.Component {
   }
 
     handleSubmit=(state) => {
-      const { createbook } = this.props;
+      const { createBook } = this.props;
       const currentTitle = state.title;
       const currentCategory = state.input;
-      createbook(state.title, state.category);
+      createBook(state.title, state.category);
       return {
         input: '',
         title: state.title.concat(currentTitle),
@@ -40,13 +39,14 @@ class BooksForm extends React.Component {
 
     render() {
       const categories = ['Action', 'Biography', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+      const { title } = this.state;
 
       return (
         <div className="container">
           <h3>Add book</h3>
 
           <form className="form-label">
-            <input value={this.state.title} onChange={this.handleChange} type="text" className="form-control" placeholder="bookname" />
+            <input value={title} onChange={this.handleChange} type="text" className="form-control" placeholder="bookname" />
             <select className="form-select" id="category" onChange={this.handleChange}>
               {categories.map((value) => (
                 <option
@@ -65,12 +65,11 @@ class BooksForm extends React.Component {
 }
 
 BooksForm.propTypes = {
-
-  createbook: PropTypes.func.isRequired,
+  createBook: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  createBook(title, category) { dispatch(createBook(title, category)); },
+  createBook: (title, category) => { dispatch(createbook(title, category)); },
 });
 
 export default connect(mapDispatchToProps)(BooksForm);
