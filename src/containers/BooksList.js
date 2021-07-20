eb92eef56add79c3bb9599b6ed2074f81b2c9754
dibdas <dibyandu08das@gmail.com>
2021-07-20 23:39:17 +0530

    modified
    added handlechangefilter

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,42 +1,58 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { removebook } from '../actions/index';
+import { removebook, changefilter } from '../actions/index';
+
 import Book from '../components/Book';
+import CategoryFilter from '../components/CategoryFilter';
 
 const BooksList = (props) => {
-  const { removebook, books } = props;
+  const { removebook, books, changefilter } = props;
+
+  const handleFilterChange = (event) => {
+    const filter = event.target.value;
+    changefilter(filter);
+  };
 
   const handleRemoveBook = (id) => {
     removebook(id);
   };
 
   return (
+    <>
+      <CategoryFilter handleFilterChange={(event) => handleFilterChange(event)} />
 
-    <div className="container">
+      <div className="container">
 
-      {books.map((book) => (
-        <Book
-          book={book}
-          key={book.Id}
-          handleRemoveBook={handleRemoveBook}
-        />
-      ))}
-    </div>
+        {books.filter((book) => book.category === filter || filter === 'All').map((book) => (
+          <Book
+            book={book}
+            key={book.Id}
+            handleRemoveBook={handleRemoveBook}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.object).isRequired,
   removebook: PropTypes.func.isRequired,
+  changefilter: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   books: state.bookreducer,
+  filter: state.filterreducer,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   removebook(Id) { dispatch(removebook(Id)); },
+  handleFilterChange(filter) { dispatch(handleFilterChange(filter)); },
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
